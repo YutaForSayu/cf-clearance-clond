84bb18815827c746d77df61bea17543482cf26c7
YutaForSayu <sayu0ogiwara81@gmail.com>
2026-04-29 14:21:17 +0000

    CFc

---
+++ main.go
@@ -0,0 +1,37 @@
+package cfClearance
+
+import (
+	"errors"
+	"github.com/YutaForSayu/cf-clearance-clond/browser"
+	"github.com/YutaForSayu/cf-clearance-clond/cfclient"
+	"github.com/YutaForSayu/cf-clearance-clond/validate"
+	"log"
+	"net/http"
+)
+
+// ConfigureCfClient should be called directly, using a URL string as the target
+// and a second string for the User-Agent to pass. User-Agent must match what
+// you use in your tooling for subsequent requests, per Cloudflare.
+// Pass in your own http.Client that will receive CloudFlares'
+// (cf_clearance) if the site is protected.
+func ConfigureClient(client *http.Client, target string, agent string) error {
+	// Initialize the client with the things we need to bypass cloudflare
+	cfclient.Initialize(client)
+
+	// Validate the target URL
+	if validate.Url(target) == false {
+		return errors.New("could not parse the target URL")
+	}
+
+	// Check if target is even protected by Cloudflare. If not, just return the
+	// client as-is.
+	if validate.CloudFlareIsPresent(target, client) == false {
+		log.Println("[*] Target not protected by Cloudflare.")
+		return nil
+	}
+
+	log.Println("[!] Target is protected by Cloudflare, bypassing...")
+
+	return browser.GetCloudFlareClearanceCookie(client, agent, target)
+
+}
